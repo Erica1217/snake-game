78cc3f5ebcc2c480b52ca7f88f412e4afe80ce42
bconfiden2 <bconfiden2@naver.com>
2020-06-05 11:47:11 +0900

    Renderable

---
+++ src/test3/GameData.cpp
@@ -0,0 +1,185 @@
+#include "GameData.h"
+#include "GameSettings.h"
+#include "kbhit.h"
+
+GameData::GameData(int stage) 
+{
+    snake_map = new SnakeMap(stage);
+    window = newwin(MAP_Y, MAP_X, 1, 1);
+    keypad(window, TRUE);
+}
+
+void GameData::update(int current_tick)
+{
+    this -> current_tick = current_tick;
+}
+
+int GameData::checkItem(Point head) 
+{
+    int info = snake_map->getPositionInfo(head.x, head.y);
+    if(info == 6) return 1;
+    if(info == 7) return -1;
+    return 0;
+}
+
+vector<vector<int>> GameData::getMap() 
+{
+    return snake_map->getCurrentMap();
+}
+
+void GameData::setNextPoint(Point next_point) 
+{
+    this -> next_point = next_point;
+}
+
+void GameData::setNextHeadPoint(Point next_head_point) 
+{
+    this -> next_head_point = next_head_point;
+}
+
+Point GameData::getNextPoint() {
+    return next_point;
+}
+
+Point GameData::getNextHeadPoint() 
+{
+    return next_head_point;
+}
+
+void GameData::setCurrentTick(int current_tick) 
+{
+    this -> current_tick = current_tick;
+}
+
+int GameData::getCurrentTick() 
+{
+    return this -> current_tick;
+}
+
+// 키값 입력될때마다 방향 변경해줌
+
+void GameData::updateDirection()
+{
+    if(kbhit())
+    {
+        key = wgetch(window);
+        switch (key)
+        {
+        case KB_W : // w
+        case KB_UP : // 위
+            current_direction = DIR_UP;
+            break;
+        case KB_S : // s
+        case KB_DOWN : // 아래
+            current_direction = DIR_DOWN;
+            break;
+        case KB_RIGHT : // d
+        case KB_D : // 오른
+            current_direction = DIR_RIGHT;
+            break;
+        case KB_A :  // a
+        case KB_LEFT : // 왼
+            current_direction = DIR_LEFT;
+            break;
+        default:
+            break;
+        }
+    }  
+}
+
+void GameData::Render()
+{
+    std::vector<std::vector<int>> canvas = snake_map -> getCurrentMap();
+  
+  for(int i = 0 ; i < canvas.size() ; i++)
+  {
+    for(int j = 0 ; j < canvas[i].size() ; j++)
+    {
+      mvwprintw(window, i, j, "%c", changeMap(canvas[i][j]));
+    }
+  }
+  wrefresh(window);
+}
+
+char GameData::changeMap(int i)
+{
+    char temp;
+  switch(i)
+  {
+    case 0 :
+      temp = ' ';
+      break;
+    case 1 :
+      temp = 'o';
+      break;
+    case 2 :
+      temp = 'x';
+      break;
+    case 3 :
+      temp = '1';
+      break;
+    case 4:
+      temp = '1';
+      break;
+    case 5:
+      temp = '%';
+      break;
+    default :
+      temp = '?';
+      break;
+  }
+  return temp;
+}
+
+
+void GameData::updateItemPosition(vector<Point> item_positions, vector<int> item_infos) 
+{
+    for(int i=0; i<item_positions.size(); i++) 
+    {
+        snake_map->setPositionInfo(item_positions[i].x, item_positions[i].y, item_infos[i]+5);
+    }
+}
+
+void GameData::updateSnakePosition(vector<Point> snake_body)
+{
+    snake_map->setPositionInfo(snake_body[0].x, snake_body[0].y,1);
+    for(int i = 1; i < snake_body.size(); i++) 
+    {
+        snake_map->setPositionInfo(snake_body[i].x, snake_body[i].y,2);
+    }
+}
+
+void GameData::updateGatePosition(bool isExist, vector<Point> gates)
+{
+    if(isExist) {
+        for(int i = 1; i < gates.size(); i++) 
+        {
+            snake_map->setPositionInfo(gates[i].x, gates[i].y,5);
+        }
+    }
+}
+
+int GameData::getCurrrentDirection() 
+{
+    return this -> current_direction;
+}
+
+void GameData::setCurrentDirection(int current_direction) 
+{
+    this -> current_direction = current_direction;
+}
+
+
+void GameData::mapReset() {
+    for(int i = 0; i <21; i++) {
+        for(int j = 0; j < 21; j ++) {
+            if(snake_map -> getPositionInfo(i,j) == 1 || snake_map -> getPositionInfo(i,j) == 2 ) {
+                snake_map -> setPositionInfo(i, j , 0);
+            }
+        }
+    }
+}
+
+int GameData::getPositionInfo(int x, int y) {
+    return snake_map -> getPositionInfo(x,y);
+}
---
+++ src/test3/GameManager.cpp
@@ -0,0 +1,87 @@
+#include "GameManager.h"
+
+GameManager::GameManager(GameFlow &gameflow) : game_flow(gameflow)
+{
+    curStage = 0;
+
+    games = new Game[MAX_STAGE];
+
+    for (int i = 0; i < MAX_STAGE; i++)
+    {
+        games[i].Init(i);
+    }
+}
+
+// 게임 시작 (첫 스테이지 시작) 시 한번
+void GameManager::Start()
+{
+    // 첫스테이지 로드
+    curGame = games;
+    curGame->gameStart();
+    // 스테이지1 시작 프롬프트
+    if (game_flow.RenderStageEnter(curStage + 1) == 0)
+    {
+        curGame->isValid = false;
+    }
+}
+
+void GameManager::End()
+{
+    curGame->gameEnd();
+}
+
+// ui_manager 가 키 입력받고 게임에 세팅해줌
+void GameManager::SetInput()
+{
+    curGame->SetInput();
+}
+
+// 매 프레임, 유효성 검사만
+int GameManager::IsValid()
+{
+    return curGame->IsValid();
+}
+
+// 매 프레임, 게임데이터 업데이트
+void GameManager::Update(int tick)
+{
+    curGame->update(tick);
+
+    if (curGame->isClear())
+    {
+        curGame->gameEnd();
+        StageSetting();
+    }
+}
+
+// 매 프레임. 게임 완료 검사 // 사실 필요 없음
+int GameManager::IsStageClear()
+{
+    //return curGame->isClear;
+}
+
+// 스테이지 클리어 시 다음 스테이지 세팅, 대기창 띄워줌
+void GameManager::StageSetting()
+{
+    // 마지막 스테이지가 아니라면
+    if (++curStage < MAX_STAGE)
+    {
+        // 다음 스테이지 로드
+        curGame = games + curStage;
+        // 다음 스테이지 세팅
+        curGame->gameStart();
+    }
+
+    // 해당 스테이지 클리어 렌더링, 입력대기
+    if (game_flow.RenderStageClear(curStage) == 0)
+    {
+        // 대기중 백스페이스 눌렀을 경우 종료
+        curGame->isValid = false;
+    }
+}
+
+// UI Manager 가 프레임마다 최종적으로 그려야 하는 데이터를 넘겨줌 (맵+뱀+게이트+아이템)
+std::vector<std::vector<int>> GameManager::GetCurMap()
+{
+    return curGame->map;
+}
