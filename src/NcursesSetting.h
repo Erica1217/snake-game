e491c43932291a91698f9a7f53cca695ee94123e
bconfiden2 <bconfiden2@naver.com>
2020-06-12 15:03:28 +0900

    fix render2

--- src/NcursesSetting.h
+++ src/NcursesSetting.h
@@ -7,21 +7,21 @@
 
 void NcursesSetting()
 {
-    //setlocale(LC_ALL, "");
-    setlocale(LC_ALL, "ko_KR.utf8");
+    setlocale(LC_ALL, "");
+    //setlocale(LC_ALL, "ko_KR.utf8");
     initscr();
     noecho();
     curs_set(0);
-    resize_term(MAP_Y + 2, MAP_X * 3 + 3);
+    resize_term(MAP_Y + 2, MAP_X * 3 + 4);
     start_color();
 
-    // init_pair(1, COLOR_WHITE, COLOR_RED);
-    // attron(COLOR_PAIR(1));
+    init_pair(1, COLOR_RED, COLOR_RED);
+    attron(COLOR_PAIR(1));
+    border('*', '*', '*', '*', '*', '*', '*', '*');
+    //border(ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+    attroff(COLOR_PAIR(1));
 
-    border(ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
-    // attroff(COLOR_PAIR(1));
-
-    mvprintw(0, 2, " Snake Game ");
+    mvprintw(0, 26, "  Snake  Game  ");
     refresh();
 }
 
